08cd35623814b4e597c5cb8005e6c0841f041640
canikwe <c.anikwe@gmail.com>
2021-05-18 20:05:35 -0400

    ✨ add sidebar module

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, MainRoutingModule],
+  imports: [CommonModule, MainRoutingModule, MatSidenavModule],
 })
 export class MainModule {}
