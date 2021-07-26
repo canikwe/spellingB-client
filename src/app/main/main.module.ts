7d6708e5fff4bda1472597015919a8cdd1709e3e
canikwe <c.anikwe@gmail.com>
2021-07-25 21:17:37 -0400

    fix existing routes

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
-
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
