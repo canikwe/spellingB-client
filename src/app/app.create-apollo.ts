a8be218ccbba7b9c09488aeaad52aa852aa09c90
canikwe <c.anikwe@gmail.com>
2021-07-25 23:47:50 -0400

    remove unused imports

--- src/app/app.create-apollo.ts
+++
@@ -1,9 +0,0 @@
-import { InMemoryCache } from '@apollo/client/core';
-import { HttpLink } from 'apollo-angular-link-http';
-
-export function createApollo(httpLink: HttpLink) {
-  return {
-    link: httpLink.create({ uri: 'https://api.example.com/graphql' }),
-    cache: new InMemoryCache(),
-  };
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
-import { APOLLO_OPTIONS } from 'apollo-angular';
-import { createApollo } from './app.create-apollo';
+import { HttpLinkModule } from 'apollo-angular-link-http';
 import { GraphQLModule } from './graphql.module';
 
 @NgModule({
