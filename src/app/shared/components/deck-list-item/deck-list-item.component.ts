db3d143ccbafb0d1b33dd79934f78451583cb3be
Chine Anikwe <c.anikwe@gmail.com>
2021-07-26 23:01:09 -0400

    ✨Dashboard Static Page Rendering (#4)
    * dashboard and deck list items components working. created shared module for reusable components
    
    * remove unused comment

---
+++ src/app/shared/components/deck-list-item/deck-list-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Deck } from '../../../../generated/graphql';
+
+@Component({
+  selector: 'app-deck-list-item',
+  templateUrl: './deck-list-item.component.html',
+  styleUrls: ['./deck-list-item.component.scss'],
+})
+export class DeckListItemComponent implements OnInit {
+  @Input() deck: Partial<Deck>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.setFakeDeck();
+  }
+
+  setFakeDeck = () => {
+    this.deck = {
+      title: 'My Deck',
+      createdAt: new Date().toLocaleDateString(),
+    };
+  };
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DeckListComponent } from './components/deck-list/deck-list.component';
+import { DeckListItemComponent } from './components/deck-list-item/deck-list-item.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [DeckListComponent, DeckListItemComponent],
+  exports: [DeckListComponent, DeckListItemComponent],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
+})
+export class SharedModule {}
