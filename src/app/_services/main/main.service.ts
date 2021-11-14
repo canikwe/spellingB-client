c92af8b4fdb09858631872faded036035c8e2d05
canikwe <c.anikwe@gmail.com>
2021-11-13 22:33:07 -0500

    rename side bar component and add routes to main service

--- src/app/_services/main/main.service.ts
+++ src/app/_services/main/main.service.ts
@@ -6,6 +6,12 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class MainService {
   public isSmallScreen = new BehaviorSubject(false);
-
+  public readonly routes = [
+    { name: 'dashboard', icon: 'home', link: '/dashboard' },
+    { name: 'play', icon: 'play_arrow', link: '/play' },
+    { name: 'add', icon: 'add_circle_outline', link: '/add' },
+    { name: 'stats', icon: 'bar_chart', link: '/stats' },
+    { name: 'more', icon: 'more_horiz', link: '/more' },
+  ];
   constructor() {}
 }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,7 +5,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
+import { SideNavComponent } from './side-bar/side-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,7 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 
 @NgModule({
-  declarations: [MainComponent, SideBarComponent, BottomNavComponent],
+  declarations: [MainComponent, SideNavComponent, BottomNavComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
