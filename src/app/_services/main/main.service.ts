ece7511c600a2e23f1a42d722434b508383d560f
canikwe <c.anikwe@gmail.com>
2021-11-13 21:58:33 -0500

    add bottom nav and main service

---
+++ src/app/_services/main/main.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MainService {
+  public isSmallScreen = new BehaviorSubject(false);
+
+  constructor() {}
+}
